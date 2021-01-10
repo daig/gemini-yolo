1091cd3d71ad4dc22f9abe80c8057638bb7773f4
Dai <daig@users.noreply.github.com>
2021-01-09 22:42:51 -0500

    better reporting

--- yolo.py
+++ yolo.py
@@ -8,6 +8,9 @@ import datetime, time
 import itertools
 import sys
 
+def wait():
+    time.sleep(0.5)
+
 [_,live_or_test] = sys.argv
 
 base_url = {"test": "https://api.sandbox.gemini.com"
@@ -18,19 +21,19 @@ priv_key = os.environ['PRIVATE_KEY'].encode()
 
 def gemini_get(endpoint):
     url = base_url + endpoint
-    print("request: GET {}".format(endpoint))
-    time.sleep(1)
+    print("\nREQUEST: GET {}".format(endpoint))
+    wait()
     resp = requests.get(url).json()
-    print("response: {}".format(resp))
-    time.sleep(0.5)
+    print("RESPONSE: {}\n".format(resp))
+    wait()
     return resp
 
 def gemini_post(endpoint,payload):
     url = base_url + endpoint
     payload_nonce = str(int(time.mktime(datetime.datetime.now().timetuple())*1000))
     payload['nonce'] = payload_nonce
-    print("request: {}".format(payload))
-    time.sleep(1)
+    print("\nREQUEST: {}".format(payload))
+    wait()
     encoded_payload = json.dumps(payload).encode()
     b64 = base64.b64encode(encoded_payload)
     signature = hmac.new(priv_key, b64, hashlib.sha384).hexdigest()
@@ -43,16 +46,17 @@ def gemini_post(endpoint,payload):
     resp = requests.post(url,
                             data=None,
                             headers=request_headers).json()
-    time.sleep(0.5)
-    print("response: {}".format(resp))
+    wait()
+    print("RESPONSE: {}\n".format(resp))
     return resp
 
-def gemini_get_balances(currency):
+def gemini_get_balances():
     """Get your current balance info for 'currency'"""
     endpoint = "/v1/balances"
     balances = gemini_post(endpoint, {"request": endpoint})
     return dict((x['currency'].lower(), x) for x in balances)
 
+    
 def gemini_market_price(buy_or_sell,pair,tick_size,margin):
     """Calculate a ask/bid limit price that will ensure a trade no more than
     'margin' times more/less than market price"""
@@ -62,23 +66,60 @@ def gemini_market_price(buy_or_sell,pair,tick_size,margin):
             "buy" : 1 + margin,
             "sell" : 1 - margin
             }[buy_or_sell]
-    price = round(float(prices[ask_or_bid]) * modifier, tick_size)
-    print("price: {}".format(price))
-    return price
+    market_price = float(prices[ask_or_bid])
+    price = round(market_price * modifier, tick_size)
+    return (market_price , price)
 
-def gemini_market(buy_or_sell,amount, asset = "btc" ,currency = "usd", margin = 0.1):
+def balance_report(balances,asset):
+    balance = balances[asset]
+    print("You have {} {}, with {} available to trade, and {} available to withdraw"
+            .format(balance['amount'],asset.upper(),balance['available'],balance['availableForWithdrawal']))
+
+def gemini_market(buy_or_sell,amount = None, cost = None, asset = "btc" ,currency = "usd", margin = 0.001):
     "Execute a buy/sell of 'asset' in 'currency' within 1 +/- 'margin' times market price"
     print(live_or_test.upper())
+
+    {'buy' : (), 'sell' : ()}[buy_or_sell]
 
     pair = (asset + currency).lower()
     print("pair: {}".format(pair))
 
+    pre_balances = gemini_get_balances()
+
     details = gemini_get("/v1/symbols/details/" + pair)
     tick_size = int(details['tick_size'])
-    amount = round(float(amount), int(details['quote_increment']))
-    print("amount: {}".format(amount))
+    quote_increment = int(details['quote_increment'])
 
-    price = gemini_market_price(buy_or_sell,pair,tick_size,margin)
+    prices = gemini_market_price(buy_or_sell,pair,tick_size,margin)
+    market_price = prices[0]
+    price = prices[1]
+
+    if not amount and cost:
+        amount = round(float(cost)/price,quote_increment)
+    elif not cost and amount:
+        amount = round(float(amount),quote_increment)
+    else:
+        raise Exception("one of 'amount' or 'cost' must be provided")
+
+    max_or_min = {"buy" : "maximum", "sell" : "minimum"}[buy_or_sell]
+    cur = currency.upper()
+    total = round(amount*price,tick_size)
+    total_expected = round(amount*market_price,tick_size)
+    print("attempting to {} {} {} at a {} of {} {} each ({} {} expected),\nfor a total {} of {} {} ({} {} expected)"
+            .format(buy_or_sell,amount,asset.upper(),
+                    max_or_min,
+                    price,cur,
+                    market_price,cur,
+                    max_or_min,
+                    total,cur,
+                    total_expected,cur
+                   )
+            )
+    balance_report(pre_balances,asset)
+    balance_report(pre_balances,currency)
+    cont = input("Continue? YES/NO\n")
+    if cont != "YES":
+        raise Exception("Aborted by user. Must confirm YES")
 
     endpoint = "/v1/order/new"
     payload = {
@@ -92,8 +133,21 @@ def gemini_market(buy_or_sell,amount, asset = "btc" ,currency = "usd", margin = 
     }
     resp = gemini_post(endpoint, payload)
     if 'reason' in resp:
-        print("FAILED: {}".format(resp['reason']))
+        if resp['reason'] == "InsufficientFunds":
+            balance = gemini_get_balances()[currency]
+            total = round(amount*price,tick_size)
+            print("Insufficient Funds: You only have {} {} available to trade (but {} total), you tried to spend {}"
+                    .format(balance['available'],currency.upper(),balance['amount'],total))
+        else:
+            print("FAILED: {}".format(resp['reason']))
+
     elif resp['remaining_amount'] == '0':
-        print("SUCCESS")
+        avg_price = float(resp['avg_execution_price'])
+        balances = gemini_get_balances()
+        total = round(amount*avg_price,tick_size)
+        print("SUCCESS: {} {} {} for {} {} each, totalling {} {}"
+                .format(buy_or_sell,amount,asset.upper(),resp['avg_execution_price'],currency.upper(),total,currency.upper()))
+        balance_report(balances,asset)
+        balance_report(balances,currency)
     else:
-        print("PARTIAL: {} unfilled".format(resp['remaining_amount']))
+        print("PARTIAL: {} {} unfilled".format(resp['remaining_amount'],asset))
